56c9032d5aa958081041095c7d62cd90ca0f23c2
justFen <fen@justfen.com>
2012-07-18 00:35:30 +0100

    chore: trimming down variables needed from user

--- Ninite/AutoNinite.ps1
+++ Ninite/AutoNinite.ps1
@@ -1,19 +1,28 @@
-## Ninite AD Deployment v0.2
-## Wrote by fen www.justfen.com // twitter.com/justfen_/
-## github.com/justfen fen@justfen.com
+## Ninite AD Deployment v0.2.1
+## github.com/justfen fen@justfen.com @justfen_
 
-## Have to import AD module for AD maniuplation
-Import-Module ActiveDirectory
+## Have to import AD module for AD manipulation
+if ((Get-Module | Where-Object { $_.Name -eq "ActiveDirectory" }) -eq $null) { Import-Module ActiveDirectory }
 
-## Variables
-$domainName = "just.fen.com"
+##################################
+# Set Variables Here #############
+##################################
+
+## The Directory in which NiniteOne sites
+$NiniteDir = "R:\Ninite\"
 ## An example would be "OU=Laptops,OU=Computers,DC=Just,DC=fen,DC=com"
 ## This would be equivalent to the following just.fen.com\Computers\Laptops
 $OU = "OU=Laptops,OU=Computers,DC=Just,DC=fen,DC=com"
-$NiniteCache = "R:\Ninite\Cache\"
-$NiniteTargets = "R:\Ninite\NiniteTargets.txt"
-$NiniteLog  = "R:\Ninite\NiniteResults.csv"
-$NiniteDir = "R:\Ninite\"
+
+##################################
+# Now set SMTP Variables Below ###
+##################################
+
+$domainLook = Get-ADForest
+$domainName = $domainLook.domains
+$NiniteCache = "$NiniteDir\Cache\"
+$NiniteTargets = "$NiniteDir\NiniteTargets.txt"
+$NiniteLog  = "$NiniteDir\NiniteResults.csv"
 $StartDate = Get-Date
 
 function MachineGenerate {
@@ -26,8 +35,6 @@ function MachineGenerate {
 }
 
 # Function to fire off confirmation eMail once it has finished running
-# Please fill in your SMTP details 
-
 function MailReport {
     $smtp = "your.smtp.server"
     $from = "ninite@yourdomain"
@@ -38,6 +45,7 @@ function MailReport {
     Send-Mailmessage -SmtpServer $smtp -From $from -To $to -Subject $subject -Body $body -Attachments $attach
 }
 
+# Actual Program
 MachineGenerate
 cmd /c NiniteOne.exe /updateonly /remote file:"$NiniteTargets" /disableshortcuts /disableautoupdate /cachepath "$NiniteCache" /silent "$NiniteLog"
 MailReport
